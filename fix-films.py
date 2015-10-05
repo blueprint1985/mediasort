148f7387568c25f504d3d55682cbaa9fdbd076b4
blueprint1985 <martinbjorling@gmail.com>
2015-10-05 22:01:16 +0200

    Create fix-films.py

---
+++ fix-films.py
@@ -0,0 +1,46 @@
+import os, sys, shutil
+
+fileexts = ["mp4", "avi", "srt"]
+
+for film in os.listdir(os.getcwd()):
+
+	if os.path.isdir(film):
+
+		newname = film.split(' (')[0]
+		os.rename(film, newname)
+		film = newname
+
+		for filename in os.listdir(os.path.abspath(film)):
+
+			if not os.path.isdir(filename):
+
+				fileext = filename.split('.')[-1]
+				oldfile = os.path.abspath(film) + "/" + filename
+
+				if fileext in fileexts:
+					
+					newfile = os.path.abspath(film) + "/" + newname + "." + fileext
+					os.rename(oldfile, newfile)
+
+				else:
+
+					os.remove(oldfile)
+
+			else:
+
+				shutil.rmtree(filename)
+
+		checksequel = film.split(' - ')
+
+		if len(checksequel) > 1:
+
+			abbreviations = [["MCU", "Marvel Cinematic Universe"]]
+
+			for abbrev in abbreviations:
+				if checksequel[0] in abbrev:
+					checksequel[0] = abbrev[1]
+					break
+
+			os.rename(film, checksequel[0])
+
+		print(newname + ": Klart")
