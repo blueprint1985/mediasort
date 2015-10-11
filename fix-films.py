a1c1cda7a6da62d691d9473ac9d24ac5dea4074f
blueprint1985 <martinbjorling@gmail.com>
2015-10-11 12:06:01 +0200

    Update fix-films.py
    Fixed bug if film is part of film series

--- fix-films.py
+++ fix-films.py
@@ -9,33 +9,8 @@ for film in os.listdir(os.getcwd()):
 	if os.path.isdir(film) and not matchFilm:
 
 		newname = film.split(' (')[0]
-		newpath = os.getcwd() + "/Filmer/" + newname
-
-		if not os.path.exists(newpath): 
-			os.makedirs(newpath)
-
 		os.rename(film, newname)
 		film = newname
-
-		for filename in os.listdir(os.path.abspath(film)):
-			
-			if not os.path.isdir(os.path.abspath(film) + "/" + filename):
-
-				fileext = filename.split('.')[-1]
-				oldfile = os.path.abspath(film) + "/" + filename
-
-				if fileext in fileexts:
-					
-					newfile = os.getcwd() + "/Filmer/" + newname + "/" + newname + "." + fileext
-					os.rename(oldfile, newfile)
-
-				else:
-
-					os.remove(oldfile)
-
-			else:
-
-				shutil.rmtree(os.path.abspath(film) + "/" + filename)
 
 		checksequel = film.split(' - ')
 
@@ -48,7 +23,34 @@ for film in os.listdir(os.getcwd()):
 					checksequel[0] = abbrev[1]
 					break
 
-			os.rename("/Filmer/" + film, checksequel[0])
+			newpath = os.getcwd() + "/Filmer/" + checksequel[0]
+
+		else:
+
+			newpath = os.getcwd() + "/Filmer/" + film			
+
+		if not os.path.exists(newpath): 
+			os.makedirs(newpath)		
+
+		for filename in os.listdir(os.path.abspath(film)):
+			
+			if not os.path.isdir(os.path.abspath(film) + "/" + filename):
+
+				fileext = filename.split('.')[-1]
+				oldfile = os.path.abspath(film) + "/" + filename
+
+				if fileext in fileexts:
+					
+					newfile = newpath + "/" + newname + "." + fileext
+					os.rename(oldfile, newfile)
+
+				else:
+
+					os.remove(oldfile)
+
+			else:
+
+				shutil.rmtree(os.path.abspath(film) + "/" + filename)
 
 		shutil.rmtree(film)
 
